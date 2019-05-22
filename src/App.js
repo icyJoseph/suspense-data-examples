79ad60b12d915ac4423597486e23a94a80d3ae23
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-05-22 14:14:13 +0200

    chore: first initial approach

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useCallback, Suspense } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+//https://codesandbox.io/s/suspense-data-fetching-297gn
+//https://codesandbox.io/s/suspense-data-fetching-end-i2ey2
+
+const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
+
+class Catch extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>An error occurs</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+let globalCache;
+
+function useData(getData, deps) {
+  if (globalCache) return globalCache;
+
+  // Suspense will catch this!
+  throw getData().then(data => {
+    globalCache = data;
+  });
+
+  // const [state, setState] = useState({
+  //   loading: true,
+  //   data: null,
+  //   error: null
+  // });
+
+  // useEffect(() => {
+  //   setState({ data: null, error: null, loading: true });
+  //   getData()
+  //     .then(data => {
+  //       setState({ data, error: null, loading: false });
+  //     })
+  //     .catch(error => {
+  //       setState({ data: null, error, loading: false });
+  //     });
+  // }, [getData]);
+
+  // return state;
+}
+
+function Data({ children }) {
+  const data = useData(async () => {
+    await delay(1000);
+
+    return "Hello World";
+  }, []);
+
+  return children(data);
+}
+
+// While our catch, error boundary, catches errors
+// Suspense will kick in when the children return a Promise!
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Catch>
+        <Suspense fallback={<div>Loading</div>}>
+          <Data>
+            {data =>
+              data && (
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    <code>{data}</code>
+                  </p>
+                </header>
+              )
+            }
+          </Data>
+        </Suspense>
+      </Catch>
     </div>
   );
 }
