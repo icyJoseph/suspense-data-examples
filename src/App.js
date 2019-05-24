f3b4da0cd351aeb09c69bcdcebc7d54fee6064b2
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-05-24 18:11:25 +0200

    feat: old approach

--- src/App.js
+++ src/App.js
@@ -1,90 +1,45 @@
-import React, { useEffect, useState, useCallback, Suspense } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-//https://codesandbox.io/s/suspense-data-fetching-297gn
-//https://codesandbox.io/s/suspense-data-fetching-end-i2ey2
+const delay = duration => new Promise(accept => setTimeout(accept, duration));
 
-const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
+const coinFlip = () => Math.random() >= 0.5;
 
-class Catch extends React.Component {
-  state = { hasError: false };
-
-  static getDerivedStateFromError(error) {
-    console.error(error);
-    return { hasError: true };
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <h1>An error occurs</h1>;
-    }
-
-    return this.props.children;
-  }
-}
-
-let globalCache;
-
-function useData(getData, deps) {
-  if (globalCache) return globalCache;
-
-  // Suspense will catch this!
-  throw getData().then(data => {
-    globalCache = data;
+function useData() {
+  const [state, setState] = React.useState({
+    error: null,
+    data: null,
+    loading: false
   });
 
-  // const [state, setState] = useState({
-  //   loading: true,
-  //   data: null,
-  //   error: null
-  // });
-
-  // useEffect(() => {
-  //   setState({ data: null, error: null, loading: true });
-  //   getData()
-  //     .then(data => {
-  //       setState({ data, error: null, loading: false });
-  //     })
-  //     .catch(error => {
-  //       setState({ data: null, error, loading: false });
-  //     });
-  // }, [getData]);
-
-  // return state;
-}
-
-function Data({ children }) {
-  const data = useData(async () => {
-    await delay(1000);
-
-    return "Hello World";
+  React.useEffect(() => {
+    setState({ error: null, loading: true, data: null });
+    delay(1000)
+      .then(() => {
+        if (coinFlip()) {
+          return setState({ error: null, loading: false, data: "Hello World" });
+        }
+        return undefined.reduce();
+      })
+      .catch(() => setState({ error: "Failure", loading: false, data: null }));
   }, []);
 
-  return children(data);
+  return state;
 }
 
-// While our catch, error boundary, catches errors
-// Suspense will kick in when the children return a Promise!
 function App() {
+  const { data, error, loading } = useData();
   return (
     <div className="App">
-      <Catch>
-        <Suspense fallback={<div>Loading</div>}>
-          <Data>
-            {data =>
-              data && (
-                <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>
-                    <code>{data}</code>
-                  </p>
-                </header>
-              )
-            }
-          </Data>
-        </Suspense>
-      </Catch>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {error && <code>Error</code>}
+          {loading && <code>Loading...</code>}
+          {data && <code>{data}</code>}
+        </p>
+      </header>
     </div>
   );
 }
